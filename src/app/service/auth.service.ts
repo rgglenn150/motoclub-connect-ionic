1c6d7a5d9e80052db2482e2a480772ff74a98e58
Ruben Glenn Madredano <rgmadredano@gmail.com>
2025-08-12 11:41:02 +0800

    Fixed login url

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-
 import { takeUntil } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private baseUrl = `${environment.apiUrl}/auth`;
   destroySubject$: Subject<void> = new Subject();
 
   constructor(private http: HttpClient) {}
 
   login(loginForm: any) {
     return this.http
-      .post('http://localhost:4200/api/auth/login', loginForm)
+      .post(`${this.baseUrl}/login`, loginForm)
       .pipe(takeUntil(this.destroySubject$));
   }
 }
