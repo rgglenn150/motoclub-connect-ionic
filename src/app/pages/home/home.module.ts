9fd3d8a16682800f6254f7f804872c8e233d5037
Ruben Glenn Madredano <rgmadredano@gmail.com>
2025-09-15 09:57:39 +0800

    Updated weather widget

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,4 +1,8 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+  NO_ERRORS_SCHEMA,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -9,11 +13,20 @@ import { HomePageRoutingModule } from './home-routing.module';
 import { HomePage } from './home.page';
 import { NotificationBellModule } from '../../components/notification-bell/notification-bell.module';
 import { WeatherWidgetModule } from '../../components/weather-widget/weather-widget.module';
+import { WeatherWidget2Module } from 'src/app/components/weather-widget2/weather-widget2.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule, NotificationBellModule, WeatherWidgetModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    NotificationBellModule,
+    WeatherWidgetModule,
+    WeatherWidget2Module,
+  ],
   exports: [HomePage],
   declarations: [HomePage],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
 export class HomePageModule {}
